db901bf1819658a700becaaca97a36d73b897488
Wojciech Trocki <wtrocki@redhat.com>
2019-08-15 14:46:31 +0100

    fix: add offix cache helpers

--- template/client/src/components/CreateTask.tsx
+++ template/client/src/components/CreateTask.tsx
@@ -1,28 +1,32 @@
 import { useMutation } from '@apollo/react-hooks';
-import { createOptimisticResponse, CacheOperation } from 'offix-client';
+import { createOptimisticResponse, CacheOperation, getUpdateFunction } from 'offix-client';
 import React from 'react';
 import { AutoFields, AutoForm, ErrorsField, SubmitField } from 'uniforms-semantic';
 import { TaskSchema } from '../graphql/GraphQLBridge';
 import { TaskMutation } from '../graphql/mutations/TaskMutation';
+import { TaskQuery } from '../graphql/queries/Tasks';
 
 export const CreateTask: React.FC = () => {
     const [createTaskMutation, { data }] = useMutation(TaskMutation, {
-        // ????
-        // optimisticResponse: createOptimisticResponse({
-        //     mutation: TaskMutation,
-        //     variables: { some_key: "some_value" },
-        //     operationType: CacheOperation.ADD,
-        //     returnType: "Task"
-        // })
-        // update: () => {
-        // },
         context: { returnType: "Task" }
     });
 
 
     return (
         <AutoForm schema={TaskSchema} onSubmit={(doc: any) =>
-            createTaskMutation({ variables: doc })} >
+            createTaskMutation({
+                variables: doc,
+                optimisticResponse: createOptimisticResponse({
+                    mutation: TaskMutation,
+                    variables: doc,
+                    operationType: CacheOperation.ADD,
+                    returnType: "Task"
+                }),
+                update: getUpdateFunction({
+                    mutationName: 'createTask',
+                    updateQuery: TaskQuery
+                })
+            })} >
             <AutoFields />
             <ErrorsField />
             <SubmitField />
