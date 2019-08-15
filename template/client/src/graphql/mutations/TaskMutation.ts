ac86a4cd69924f6512e58be5470891c98e42cf4d
Wojciech Trocki <wtrocki@redhat.com>
2019-08-15 15:41:41 +0100

    fix: Reorder mutation

--- template/client/src/graphql/mutations/TaskMutation.ts
+++ template/client/src/graphql/mutations/TaskMutation.ts
@@ -2,11 +2,11 @@ import gql from "graphql-tag";
 import { TaskFragment } from "../fragments/Task";
 
 export const TaskMutation = gql`
-${TaskFragment}
-
-mutation createTask($title: String!, $description: String,  $version: String) {
+mutation createTask($title: String!, $description: String,  $version: Int) {
     createTask(input: {title: $title, description: $description, version: $version, status: "CREATED"}) {
        ...TaskFields
     }
   }
+
+${TaskFragment}
 `
