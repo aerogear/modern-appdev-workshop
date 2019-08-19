ab7f9efeed2632aa24b199fa030ffad1c6d9665f
Wojciech Trocki <wtrocki@redhat.com>
2019-08-19 15:15:30 +0100

    fix: test

--- template/client/src/graphql/mutations/TaskMutation.ts
+++ template/client/src/graphql/mutations/TaskMutation.ts
@@ -11,12 +11,3 @@ mutation createTask($title: String!, $description: String) {
 ${TaskFragment}
 `
 
-export const AssignTaskMutation = gql`
-mutation assign($id: ID!, $status: String!, $version: Int!) {
-  assign(id: $id, status: $status, version: $version) {
-    id,
-    status,
-    version
-  }
-}
-`
